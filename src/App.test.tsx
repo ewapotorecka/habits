50f9730a53fc6e56ddac1de4da3043ef35977cc3
Ewa Potorecka <ewa.potorecka@thirty3.ch>
2023-03-29 14:28:35 +0200

    added storage provider to test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 test("renders learn react link", () => {
   render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   );
   const linkElement = screen.getByText(/start here/i);
   expect(linkElement).toBeInTheDocument();
