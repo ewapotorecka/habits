be0e1829ceb52020d784d04ab9a1a3c255f08280
Ewa Potorecka <ewa.potorecka@thirty3.ch>
2023-04-03 14:22:01 +0200

    added formik and yup, added form validation

--- src/components/Pages/Form/Form.tsx
+++ src/components/Pages/Form/Form.tsx
@@ -17,62 +17,82 @@ import { setNewHabit } from "../../../app/features/habit/habitSlice";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import { Habit } from "../../../app/features/habit/habitTypes";
 import lightFormat from "date-fns/lightFormat";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const validationSchema = yup.object({
+  goal: yup.string().required("Goal is required"),
+  schema: yup.string().required("Schema is required"),
+  rewards: yup.array().min(1, "Add at least one reward"),
+});
 
 const HabitForm = () => {
-  const [rewards, setRewards] = useState<{ label: string; id: number }[]>([]);
   const [rewardInput, setRewardInput] = useState("");
-  const [goalInput, setGoalInput] = useState("");
-  const [schemaInput, setSchemaInput] = useState("");
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      goal: "",
+      schema: "",
+      rewards: [],
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      const newHabit: Habit = {
+        goal: values.goal,
+        schema: values.schema,
+        rewards: values.rewards,
+        id: crypto.randomUUID(),
+        habitStrength: {
+          strength: 0,
+          history: [],
+        },
+        data: [...new Array(30)].map(() => false),
+        startDate: lightFormat(new Date(), "yyyy-MM-dd"),
+      };
+      dispatch(setNewHabit(newHabit));
+      navigate("/");
+    },
+  });
 
   return (
     <Box sx={{ padding: "4rem", maxWidth: "50%" }}>
-      <form
-        onSubmit={(e) => {
-          const newHabit: Habit = {
-            goal: goalInput,
-            schema: schemaInput,
-            rewards,
-            id: crypto.randomUUID(),
-            habitStrength: {
-              strength: 0,
-              history: [],
-            },
-            data: [...new Array(30)].map(() => false),
-            startDate: lightFormat(new Date(), "yyyy-MM-dd"),
-          };
-          e.preventDefault();
-          dispatch(setNewHabit(newHabit));
-          navigate("/");
-        }}
-      >
+      <form onSubmit={formik.handleSubmit}>
         <FormGroup sx={{ display: "flex", gap: "2rem" }}>
           <FormControl>
-            <InputLabel htmlFor="goal-input">Define your goal</InputLabel>
+            <InputLabel htmlFor="goal">Define your goal</InputLabel>
             <Input
-              id="goal-input"
-              aria-describedby="goal-input-helper-text"
-              value={goalInput}
-              onChange={(e) => setGoalInput(e.target.value)}
+              id="goal"
+              name="goal"
+              aria-describedby="goal-helper-text"
+              value={formik.values.goal}
+              onChange={formik.handleChange}
+              error={formik.touched.goal && Boolean(formik.errors.goal)}
             />
-            <FormHelperText id="goal-input-helper-text">
-              Keep it simple and specific.
+
+            <FormHelperText id="goal-helper-text">
+              {!formik.errors.goal
+                ? "Keep it simple and specific."
+                : formik.touched.goal && formik.errors.goal}
             </FormHelperText>
           </FormControl>
           <FormControl>
-            <InputLabel htmlFor="schema-input">
+            <InputLabel htmlFor="schema">
               Define schema, that will help you perform your habit
             </InputLabel>
             <Input
-              id="schema-input"
-              aria-describedby="schema-input-helper-text"
-              value={schemaInput}
-              onChange={(e) => setSchemaInput(e.target.value)}
+              id="schema"
+              name="schema"
+              aria-describedby="schema-helper-text"
+              value={formik.values.schema}
+              onChange={formik.handleChange}
+              error={formik.touched.schema && Boolean(formik.errors.schema)}
             />
-            <FormHelperText id="schema-input-helper-text">
-              Plan when and where you will do your chosen action. Be consistent:
-              choose a time and place that you encounter every day of the week.
+            <FormHelperText id="schema-helper-text">
+              {!formik.errors.schema
+                ? "Plan when and where you will do your chosen action. Be consistent: choose a time and place that you encounter every day of the week."
+                : formik.touched.schema && formik.errors.schema}
             </FormHelperText>
           </FormControl>
 
@@ -81,7 +101,8 @@ const HabitForm = () => {
               Think about rewards. Add as many as you like.
             </InputLabel>
             <Input
-              id="reward"
+              id="rewards"
+              name="rewards"
               aria-describedby="my-helper-text"
               value={rewardInput}
               onChange={(e) => setRewardInput(e.target.value)}
@@ -89,8 +110,8 @@ const HabitForm = () => {
                 if (e.key === "Enter") {
                   e.preventDefault();
 
-                  setRewards([
-                    ...rewards,
+                  formik.setFieldValue("rewards", [
+                    ...formik.values.rewards,
                     {
                       label: rewardInput,
                       id: Math.floor(Math.random() * 100000),
@@ -103,8 +124,8 @@ const HabitForm = () => {
                 <IconButton
                   color="primary"
                   onClick={() => {
-                    setRewards([
-                      ...rewards,
+                    formik.setFieldValue("rewards", [
+                      ...formik.values.rewards,
                       {
                         label: rewardInput,
                         id: Math.floor(Math.random() * 100000),
@@ -117,6 +138,9 @@ const HabitForm = () => {
                 </IconButton>
               }
             />
+            <FormHelperText id="rewards-helper-text">
+              {formik.touched.rewards && formik.errors.rewards}
+            </FormHelperText>
           </FormControl>
           <Box
             sx={{
@@ -127,19 +151,24 @@ const HabitForm = () => {
             }}
           >
             <Typography variant="body1">Rewards:</Typography>
-            {rewards.map((reward) => (
-              <Chip
-                label={reward.label}
-                key={reward.id}
-                onDelete={() =>
-                  setRewards(
-                    rewards.filter(
-                      (rewardToDelete) => rewardToDelete.id !== reward.id
-                    )
-                  )
-                }
-              />
-            ))}
+            {formik.values.rewards.length > 0 &&
+              formik.values.rewards.map(
+                (reward: { label: string; id: number }) => (
+                  <Chip
+                    label={reward.label}
+                    key={reward.id}
+                    onDelete={() =>
+                      formik.setFieldValue(
+                        "rewards",
+                        formik.values.rewards.filter(
+                          (rewardToDelete: { id: number; label: string }) =>
+                            rewardToDelete.id !== reward.id
+                        )
+                      )
+                    }
+                  />
+                )
+              )}
           </Box>
         </FormGroup>
         <Button type="submit" variant="contained">
